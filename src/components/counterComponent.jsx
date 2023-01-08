ba9444d330df387e75ee48343f8b88397655851c
GraceUmutesi <tesigrace98@gmail.com>
2023-01-08 15:26:57 +0200

    passing data to components

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state={
-        count:0,
+        value:this.props.value,
         tags:["tag1","tag2","tag3"]
     };
     //updating the state property, binding with this solution1
@@ -17,7 +17,7 @@ class Counter extends Component {
         /*note: in react the state is not modified directly,
          that is why setState method needs to be used to sync the virtual DOM
          and the actual DOM*/
-        this.setState({count:this.state.count +1})
+        this.setState({value:this.state.value +1})
     }
     // handleIncrements(){
     //     console.log("Increment clicked",this)
@@ -27,7 +27,7 @@ class Counter extends Component {
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={()=>this.handleIncrements(this.state.count)} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={()=>this.handleIncrements(this.state.value)} className="btn btn-secondary btn-sm">Increment</button>
                 <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
             </div>
         );
@@ -35,14 +35,14 @@ class Counter extends Component {
     }
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ?
+        classes += this.state.value === 0 ?
             "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const{ count }= this.state;
-        return count ===0 ? "Zero": count;
+        const{ value }= this.state;
+        return value ===0 ? "Zero": value;
     }
 }
 
